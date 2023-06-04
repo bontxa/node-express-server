94b4f9f8d3f861e87581f42bdb7c48bb729c86e2
bontxa <alessioboncinelli@gmail.com>
2023-06-04 18:57:01 +0200

    aggiunto file .env e semplificato codice

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const { error } = require('console')
 var express = require ('express')
 var app = express()
 
-var postgres = require ('pg')
-const { Pool } = postgres
+require ('dotenv').config()
+
+const { Pool } = require ('pg')
 const pool = new Pool({
-    user:       'postgres',
-    host:       'db',
-    database:   'postgres',
-    password:   'postgres',
-    port:       5432,
+    user:       process.env.DB_USER,
+    host:       process.env.DB_HOST,
+    database:   process.env.DB_NAME,
+    password:   process.env.DB_PASS,
+    port:       process.env.DB_PORT,
 })
 
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static('html'))
+app.use(express.static('css'))
 
-app.get('/', function(req, res) { res.sendFile(__dirname + '/html/index.html')})
-app.get('/about', function(req, res) { res.sendFile(__dirname + '/html/about.html')})
-app.get('/form', function(req, res) { res.sendFile(__dirname + '/html/form.html')})
-app.get('/success', function(req, res) { res.sendFile(__dirname + '/html/success.html')})
-app.get('/sendquery', function(req, res) { res.sendFile(__dirname + '/html/sendquery.html')})
-app.get('/css', function(req, res) { res.sendFile(__dirname + '/css/style.css')})
 app.post('/submit-form', (req, res) => { 
     const { nome, cognome, email } = req.body
     const query = 'INSERT INTO users (nome, cognome, email) VALUES ($1, $2, $3)'
@@ -27,7 +23,7 @@ app.post('/submit-form', (req, res) => {
 
     pool.query(query, values, (error, result) => {
         if (error) {
-            res.status(500).send('query fallita con il seguente errore:\n', + error.message)
+            res.status(500).send('query fallita con il seguente errore:\n' + error.message)
         }
         res.status(200).sendFile(__dirname + '/html/success.html')
     })
@@ -36,11 +32,10 @@ app.post('/submit-query', (req, res) => {
     const query = req.body.query
     pool.query(query, (error, result) => {
         if (error) {
-            res.status(500).send('query fallita con il seguente errore:\n', + error.message)
+            res.status(500).send('query fallita con il seguente errore:\n' + error.message)
         }
         const tmp = result.rows
         res.status(200).send(JSON.stringify(tmp, null, 2))
-        //res.status(200).json(result.rows)
     })
 })
 
